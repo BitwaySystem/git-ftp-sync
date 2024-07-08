582b9627fda327bc3cc3785d4ae2604f9d08f1be
Fernando Santiago <fernando.santiago@bitwaysystem.com.br>
2024-07-08 05:21:02 -0300

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,26 +26,30 @@ cat changed_files.txt
 echo "Files deleted since last commit:"
 cat deleted_files.txt
 
-echo "Filtering existing files..."
-grep -Fx -f <(find . -type f | sed 's|^\./||') changed_files.txt > existing_files.txt
-echo "Existing files to be archived:"
-cat existing_files.txt
+if [ -s changed_files.txt ]; then
+  echo "Filtering existing files..."
+  grep -Fx -f <(find . -type f | sed 's|^\./||') changed_files.txt > existing_files.txt
+  echo "Existing files to be archived:"
+  cat existing_files.txt
 
-echo "Creating tar.gz archive of modified files..."
-tar -czf changed_files.tar.gz -T existing_files.txt
-echo "Archive created: changed_files.tar.gz"
+  echo "Creating tar.gz archive of modified files..."
+  tar -czf changed_files.tar.gz -T existing_files.txt
+  echo "Archive created: changed_files.tar.gz"
 
-echo "Uploading tar.gz file to FTP server..."
-lftp -u $FTP_USER,$FTP_PASS $FTP_HOST -e "set ftp:ssl-force true; set ssl:verify-certificate false; put changed_files.tar.gz -o changed_files.tar.gz; bye"
-echo "File uploaded to FTP server."
+  echo "Uploading tar.gz file to FTP server..."
+  lftp -u $FTP_USER,$FTP_PASS $FTP_HOST -e "set ftp:ssl-force true; set ssl:verify-certificate false; put changed_files.tar.gz -o changed_files.tar.gz; bye"
+  echo "File uploaded to FTP server."
 
-if [ "$EXTRACT_PATH" != "/" ]; then
-  EXTRACT_PATH=${EXTRACT_PATH%/}  # Remove trailing slash if exists
+  if [ "$EXTRACT_PATH" != "/" ]; then
+    EXTRACT_PATH=${EXTRACT_PATH%/}  # Remove trailing slash if exists
+  fi
+
+  echo "Extracting tar.gz file on FTP server via SSH..."
+  sshpass -p $FTP_PASS ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR $FTP_USER@$FTP_HOST "tar -xzf changed_files.tar.gz -C $EXTRACT_PATH && rm -f changed_files.tar.gz"
+  echo "Extraction completed on FTP server."
+else
+  echo "No modified files to archive and upload."
 fi
-
-echo "Extracting tar.gz file on FTP server via SSH..."
-sshpass -p $FTP_PASS ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR $FTP_USER@$FTP_HOST "tar -xzf changed_files.tar.gz -C $EXTRACT_PATH && rm -f changed_files.tar.gz"
-echo "Extraction completed on FTP server."
 
 if [ -s deleted_files.txt ]; then
   echo "Deleting files on FTP server..."
