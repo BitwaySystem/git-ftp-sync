607c4b0afffd60c5af760103f588c81c35a84476
Fernando Santiago <fernando.santiago@bitwaysystem.com.br>
2024-07-08 06:05:00 -0300

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -59,7 +59,9 @@ if [ -s changed_files.txt ]; then
 
   echo "Extracting tar.gz file on FTP server via SSH..."
   if command -v sshpass &> /dev/null; then
-    sshpass -p $FTP_PASS ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR $FTP_USER@$FTP_HOST "tar -xzf changed_files.tar.gz -C $EXTRACT_PATH && rm -f changed_files.tar.gz"
+    sshpass -p $FTP_PASS ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR $FTP_USER@$FTP_HOST "
+      tar -xzf changed_files.tar.gz -C $EXTRACT_PATH &&
+      rm -f changed_files.tar.gz"
     echo "Extraction completed on FTP server."
   else
     echo "sshpass not found. Please ensure sshpass is installed."
@@ -71,8 +73,9 @@ fi
 
 if [ -s deleted_files.txt ]; then
   echo "Deleting files on FTP server..."
-  if command -v sshpass &> /dev/null; then
-    sshpass -p $FTP_PASS ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR $FTP_USER@$FTP_HOST "cat deleted_files.txt | xargs -I {} rm -f {}"
+  if command -v sshpass &> /dev/null; então
+    sshpass -p $FTP_PASS ssh -o StrictHostKeyChecking=no -o LogLevel=ERROR $FTP_USER@$FTP_HOST "
+      cat deleted_files.txt | xargs -I {} rm -f {}"
     echo "Deleted files on FTP server."
   else
     echo "sshpass not found. Please ensure sshpass is installed."
